1a52c70804d548506caf4ac9f280ef15bdf5d7fd
Roma Niyazov <romaniyazov@gmail.com>
2019-09-24 17:07:50 +0300

    levels!!

--- src/bfs.c
+++ src/bfs.c
@@ -6,38 +6,54 @@
 /*   By: adavis <adavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 13:15:40 by adavis            #+#    #+#             */
-/*   Updated: 2019/09/24 15:27:08 by adavis           ###   ########.fr       */
+/*   Updated: 2019/09/24 17:03:58 by adavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include <stdio.h>
 
+int		q_len(t_queue *q)
+{
+	int		cnt;
+
+	cnt = 0;
+	while (q)
+	{
+		cnt++;
+		q = q->next;
+	}
+	return (cnt);
+}
+
 void	bfs(t_lemin *lem)
 {
 	t_queue	*q;
 	int		i;
+	int		j;
 	int		start;
-	int		level;
 	int		current;
 
-	printf("%d\n", start = lem->start->index);
-	i = 0;
-	while (i < lem->size)
-	{
-		if (lem->smezh[start][i])
-			printf("Child: %d\n", i);
-		i++;
-	}
+	q = NULL;
 	push_queue(&q, lem->start->index);
-	level = 0;
+	lem->start->level = 0;
+	lem->end->level = 2147483647;
+	lem->end->visit = 1;
 	while (q)
 	{
 		current = pop_queue(&q);
-		find_room(current, lem)->room->visit = 1;
-		find_room(current, lem)->room->level = level;
+		printf("current: %s\n", find_room_ind(current, lem)->name);
+		find_room_ind(current, lem)->visit = 1;
 		i = 0;
-		while (i <)
-		level++;
+		while (i < lem->size)
+		{
+			if (lem->smezh[current][i] && !(find_room_ind(i, lem)->visit))
+			{
+				push_queue(&q, i);
+				if (find_room_ind(i, lem)->level == -1)
+					find_room_ind(i, lem)->level = find_room_ind(current, lem)->level + 1;
+			}
+			i++;
+		}
 	}
 }
