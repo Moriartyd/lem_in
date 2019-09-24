e45fe03d8deb0f3131955433fed5a112aad51a5d
Roma Niyazov <romaniyazov@gmail.com>
2019-09-24 15:16:58 +0300

    :)

--- src/bfs.c
+++ src/bfs.c
@@ -6,18 +6,25 @@
 /*   By: adavis <adavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 13:15:40 by adavis            #+#    #+#             */
-/*   Updated: 2019/09/24 14:38:53 by adavis           ###   ########.fr       */
+/*   Updated: 2019/09/24 15:04:30 by adavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include <stdio.h>
 
-void	bfs(t_rooms *rooms, t_room *start)
+void	bfs(t_lemin *lem)
 {
 	t_queue	*q;
+	int		i;
+	int		start;
 
-	rooms = rooms->next;
-	push_queue(&q, start->index);
-	printf("AAAAAAA%d\n", pop_queue(&q));
+	printf("%d\n", start = lem->start->index);
+	i = 0;
+	while (i < lem->size)
+	{
+		if (lem->smezh[start][i])
+			printf("Child: %d\n", i);
+		i++;
+	}
 }
