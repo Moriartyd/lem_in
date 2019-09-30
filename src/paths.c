7d5e6be88910f49ac664b503693f0105329b1df2
Roma Niyazov <romaniyazov@gmail.com>
2019-09-30 21:28:29 +0300

    paths

--- src/paths.c
+++ src/paths.c
@@ -6,46 +6,97 @@
 /*   By: adavis <adavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 21:44:23 by cpollich          #+#    #+#             */
-/*   Updated: 2019/09/30 14:47:10 by adavis           ###   ########.fr       */
+/*   Updated: 2019/09/30 21:27:32 by adavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
 /*
-**	A better pathfinder
+**	Debug print
 */
 
-void	follow_path(t_lemin *lem, int i)
+void	print_paths(int **paths, t_lemin *lem);
+
+/*
+**	A good (?) pathfinder
+*/
+
+int		pathscnt(t_lemin *lem, int start)
+{
+	int		i;
+	int		cnt;
+
+	cnt = 0;
+	i = -1;
+	while (++i < lem->size)
+		if (lem->smezh[start][i])
+			cnt++;
+	return (cnt);
+}
+
+int		*follow_path(t_lemin *lem, int i)
 {
 	int		j;
+	int		p;
+	int		*path;
 
+	path = (int *)malloc(sizeof(int) * (pathlen(lem, i) + 1));
 	j = -1;
+	p = 0;
 	while (++j < lem->size && i != lem->end->index)
 	{
 		if (lem->smezh[i][j])
 		{
-			printf("%s->", find_room_ind(i, lem)->name);
+			path[p] = j;
+			p++;
 			i = j;
 			j = -1;
 		}
 	}
-	printf("%s\n", find_room_ind(i, lem)->name);
+	path[p] = -1;
+	return (path);
 }
 
 void	create_paths(t_lemin *lem)
 {
 	int		i;
+	int		p;
 	int		start;
+	int		**paths;
 
 	start = lem->start->index;
+	paths = (int**)malloc(sizeof(int *) * pathscnt (lem, start));
 	i = -1;
+	p = 0;
 	while (++i < lem->size)
 	{
 		if (lem->smezh[start][i])
 		{
-			printf("\n");
-			follow_path(lem, i);
+			paths[p] = follow_path(lem, i);
+			p++;
 		}
+	}
+	print_paths(paths, lem);
+}
+
+/*
+**	Debug print
+*/
+
+void	print_paths(int **paths, t_lemin *lem)
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i < pathscnt(lem, lem->start->index))
+	{
+		j = -1;
+		while (paths[i][++j] != -1)
+		{
+			printf("%s ", find_room_ind(paths[i][j], lem)->name);
+		}
+		printf("\n");
 	}
 }
