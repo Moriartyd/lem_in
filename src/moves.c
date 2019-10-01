4a55cfae94b60395755c8a18c5cc2e889d343714
Roma Niyazov <romaniyazov@gmail.com>
2019-10-01 15:44:20 +0300

    No more spaces

--- src/moves.c
+++ src/moves.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   moves.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpollich <cpollich@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adavis <adavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 13:25:43 by adavis            #+#    #+#             */
-/*   Updated: 2019/10/01 15:17:12 by cpollich         ###   ########.fr       */
+/*   Updated: 2019/10/01 15:43:18 by adavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ void	perform_move(t_ant *ants, int **paths, t_lemin *lem)
 					;
 			ants[i].room = paths[ants[i].path][j];
 			ft_printf("L%d-%s", i + 1, find_room_ind(ants[i].room, lem)->name);
-			if (i < lem->ants)
+			if (ants[i + 1].path >= 0 && i + 1 < lem->ants)
 				ft_printf(" ");
 		}
 	}
